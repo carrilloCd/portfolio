1411c8794283ca21672ad5ab77e6f1395fb990ce
Carlos Daniel Carrillo <carloscarrillo238@gmail.com>
2025-02-21 10:28:22 -0500

    feat(hero-section): add HeroSection with CTA buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from '@components/layout';
+import { HeroSection } from '@components/sections';
 
 export function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   )
 }
---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import './Button.scss'
+
+interface IButtonProps {
+  url?: string;
+  icon?: ReactNode;
+  text?: string;
+  variant?: 'primary' | 'icon';
+}
+export const Button: React.FC<IButtonProps> = ({ icon, text, variant, url }) => {
+  const ButtonContent = (
+    <>
+      {text && <span>{text}</span>}
+      {icon && <span>{icon}</span>}
+    </>
+  )
+
+  if (url) {
+    return (
+      <a href={url} className={`button button--${variant}`} target='_blank'>
+        {ButtonContent}
+      </a>)
+  }
+
+  return (
+    <button className={`button ${variant}`}>
+      {ButtonContent}
+    </button>
+  )
+
+}
